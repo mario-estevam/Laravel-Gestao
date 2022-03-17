3da218dd6fd95d73f71d03fc1992ce4f5d4ed2a1
mario-estevam <mario.neto47@gmail.com>
2022-03-17 13:45:32 -0300

    Implementando SoftDelet

--- database/migrations/2022_03_17_163946_alter_fornecedors_nova_coluna.php
+++ database/migrations/2022_03_17_163946_alter_fornecedors_nova_coluna.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFornecedorsTable extends Migration
+class AlterFornecedorsNovaColuna extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateFornecedorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('fornecedors', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome',50);
-            $table->timestamps();
-
+        Schema::table('fornecedors', function (Blueprint $table) {
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +25,6 @@ class CreateFornecedorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fornecedors');
+        //
     }
 }
