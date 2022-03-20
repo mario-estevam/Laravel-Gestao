96954814c861a384c86f15cfa9bbdab52f494c71
mario-estevam <mario.neto47@gmail.com>
2022-03-20 15:26:16 -0300

    Recuperando dados do formulário e persistindo no banco;

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request  $request)
     {
-        var_dump($_POST);
+        $contato = new SiteContato();
+        /*
+         * podemos obter e persistir os dados assim
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+         $contato->save();
+        */
+
+        // ou dessa maneira mais enxuta
+        $contato->create($request->all());
+
         return view('site.contato', ['titulo'=>'Contato']);
     }
 }
